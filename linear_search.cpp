6b19fe297a0a5c786a3ba14be393a68dcc9e001c
ruthivikreddy-04 <kudumularuthivikreddy.131456@marwadiuniversity.ac.in>
2026-08-13 15:29:21 +0530

    Create linear_search.cpp

---
+++ linear_search.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n = 5;
+int key = 4;
+int arr[5] = { 1,2,3,4,5};
+for(int i = 0;i<n;i++){
+if(arr[i] == key ){
+cout<<"element present index "<<i<<endl;
+}
+}
+}
